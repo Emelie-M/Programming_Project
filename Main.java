e28658eba03806083757ba8c3aee528691fa224f
emelie <ememejia2106@gmail.com>
2025-04-28 09:39:31 -0400

    3 key methods

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+    public void addAthlete(String Name, int DOB, double weight, String username)
+    {
+        //user creates an account
+    }
+    
+    public void listAllAthletes()
+    {
+        // List all register athletes
+        // prints all athletes
+    }
+    
+    public void listExistingActivities()
+    {
+        //Gets all activities on the Activities class and prints them
+    }
+}
